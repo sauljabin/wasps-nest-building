7420696ce8b6161e7597d63e0037ddde6c492e20
Saúl Piña <sauljabin@gmail.com>
2014-04-16 19:31:20 -0530

    Mejora

---
+++ src/app/simulation/Rule.java
@@ -0,0 +1,48 @@
+package app.simulation;
+
+import java.awt.Color;
+
+public class Rule {
+	private int state;
+	private Color color;
+	private String neighbourhood;
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public String getNeighbourhood() {
+		return neighbourhood;
+	}
+
+	public void setNeighbourhood(String neighbourhood) {
+		this.neighbourhood = neighbourhood;
+	}
+
+	public Rule() {
+	}
+
+	public Rule(String neighbourhood, int state, Color color) {
+		this.state = state;
+		this.color = color;
+		this.neighbourhood = neighbourhood;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Rule [neighbourhood=%s, state=%s, color=[r=%s, g=%s, b=%s]]", neighbourhood, state, color.getRed(), color.getGreen(), color.getGreen());
+	}
+
+}
