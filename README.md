ff6737b1fa1e92cb27e73dd37d5e8d01698a7d9f
Saúl Piña <sauljabin@gmail.com>
2016-03-16 18:01:46 -0500

    Actualizadas cabeceras

---
+++ README.md
@@ -0,0 +1,24 @@
+WaspsNestBuilding
+=================
+
+Descripción
+-----------
+Inspirados en el comportamiento de las avispas al construir sus
+nidos, Theraulaz y Bonabeau proponen el algoritmo de construcción
+de nido de avispas, éste considera a un grupo de agentes que se
+mueven de forma asincrónica y aleatoria en un espacio discreto en
+tres dimensiones y que se comportan a nivel local como máquinas de
+estímulo-respuesta. Cada agente contribuye a la construcción de
+una estrutura regida por micro-reglas.
+
+- Copyright: 2014 Saúl Piña <sauljabin@gmail.com>
+- Repository: https://github.com/sauljabin/wasps-nest-building
+- License: MIT
+- Language: Java
+- IDE: Eclipse
+
+
+Screenshots
+-----------
+![](/documents/screenshot-1.png)
+![](/documents/screenshot-2.png)
--- src/app/simulation/Rule.java
+++ src/app/simulation/Rule.java
@@ -1,21 +1,10 @@
 /**
+ * Copyright (c) 2014 Saúl Piña <sauljabin@gmail.com>.
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This file is part of WaspsNestBuilding.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
- *		SAUL PIÑA - SAULJP07@GMAIL.COM
- *		2014
+ * WaspsNestBuilding is licensed under The MIT License.
+ * For full copyright and license information please see the LICENSE file.
  */
 
 package app.simulation;
